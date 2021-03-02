efb273560a43affc28eade3b3379ff6762ac4039
Tim Keller <timothy.m.keller@gmail.com>
2021-03-02 00:16:53 -0800

    Functional kitty with tutorial boilerplate.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef, useState } from "react";
+//R3F
+import { Canvas, useFrame } from "react-three-fiber";
+// Deai - R3F
+import { softShadows, MeshWobbleMaterial, useGLTF } from "@react-three/drei";
+// Styles
+import "./App.css";
+// React Spring
+import { useSpring, a } from "react-spring/three";
 
-function App() {
+// soft Shadows
+softShadows();
+
+function KittyA(props) {
+
+  const group = useRef();
+
+  useFrame(() => {
+    group.current.rotation.y = 0.001 * props.mouseX;
+    group.current.rotation.x = 0.001 * props.mouseY;
+  })
+  const { nodes, materials } = useGLTF('/3d/KittyA.gltf')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <group ref={group} {...props} dispose={null}>
+      <group position={[0, 0, 0]}>
+          <mesh material={materials.blinn1} geometry={nodes.KittyA_MSHPIV.geometry} position={[0, 0, 0]} />
+      </group>
+      </group>
+  )
 }
+
+const SpinningMesh = ({ position, color, speed, args }) => {
+  //ref to target the mesh
+  const mesh = useRef();
+
+  //useFrame allows us to re-render/update rotation on each frame
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  //Basic expand state
+  const [expand, setExpand] = useState(false);
+  // React spring expand animation
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+  });
+  return (
+    <a.mesh
+      position={position}
+      ref={mesh}
+      onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow>
+      <boxBufferGeometry attach='geometry' args={args} />
+      <MeshWobbleMaterial
+        color={color}
+        speed={speed}
+        attach='material'
+        factor={0.6}
+      />
+    </a.mesh>
+
+    //Using Drei box if you want
+    // <Box {...props} ref={mesh} castShadow>
+    //   <MeshWobbleMaterial
+    //     {...props}
+    //     attach='material'
+    //     factor={0.6}
+    //     Speed={1}
+    //   />
+    // </Box>
+  );
+};
+
+const App = () => {
+  const [ mouseX, setMouseX ] = useState(0);
+  const [ mouseY, setMouseY ] = useState(0);
+
+  return (
+    <>
+      <p>X: { mouseX }, Y: { mouseY }</p>
+      <Canvas
+        onMouseMove={ ( e ) => { 
+          setMouseX( e.clientX );
+          setMouseY( e.clientY );
+        }}
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 60 }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color='lightblue'
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color='pink' speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color='pink' speed={6} />
+        </group>
+        <Suspense fallback={ null } >
+          <KittyA mouseX={ mouseX } mouseY={ mouseY } position={[0, 5, 0]}/>
+        </Suspense>
+      </Canvas>
+    </>
+  );
+};
 
 export default App;
